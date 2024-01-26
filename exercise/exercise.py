56989b148f5f1c3b91e7297195d8426d2e6bfc8f
Mauro-Prencipe <49814184+Mauro-Prencipe@users.noreply.github.com>
2024-01-26 09:29:58 +0100

    Add files via upload

--- exercise/exercise.py
+++ exercise/exercise.py
@@ -1,27 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import random
 from scipy.optimize import curve_fit
 
+# load x, y data from the file 'exercise_data.dat' 
+# and fit them by using a third-degree polynomials (polyfit)
+# and also by a third-degree polynomial function not having
+# the second-degree term: 
+#      y = ax^3 + bx + c
 
-class Data():
-
-    def __init__(self, x):
-        self.x=x
-        self.err=[random.gauss(0., sigma=0.1) for ie in range(len(x))]
-        self.y = self.compute_y()+self.err
-                      
-    def compute_y(self):
-        y=self.x**3 - 0.8*self.x**2 +2*self.x + 4
-        return y 
-        
-
-x=np.array([-4, -3, -2., -1., 0., 1., 2., 3.])
-d=Data(x)
-
-
-def cubic(x, a, b, c):
-    return a*x**3 + b*x + c
 
 
 
