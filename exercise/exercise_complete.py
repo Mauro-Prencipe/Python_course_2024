4d4254d80048e27f87eab1291f5124c242bea85f
Mauro-Prencipe <49814184+Mauro-Prencipe@users.noreply.github.com>
2024-01-26 09:32:53 +0100

    Add files via upload

--- exercise/exercise_complete.py
+++ exercise/exercise_complete.py
@@ -3,21 +3,9 @@ import matplotlib.pyplot as plt
 import random
 from scipy.optimize import curve_fit
 
-
-class Data():
-
-    def __init__(self, x):
-        self.x=x
-        self.err=[random.gauss(0., sigma=0.1) for ie in range(len(x))]
-        self.y = self.compute_y()+self.err
-                      
-    def compute_y(self):
-        y=self.x**3 - 0.8*self.x**2 +2*self.x + 4
-        return y 
-        
-
-x=np.array([-4, -3, -2., -1., 0., 1., 2., 3.])
-d=Data(x)
+data=np.loadtxt('exercise_data.dat')
+x=data[:,0]
+y=data[:,1]
 
 
 def cubic(x, a, b, c):
@@ -25,8 +13,8 @@ def cubic(x, a, b, c):
 
 
 def fit():
-    fit_poly=np.polyfit(d.x,d.y,3)
-    fit_curve=curve_fit(cubic, d.x, d.y)
+    fit_poly=np.polyfit(x,y,3)
+    fit_curve=curve_fit(cubic, x, y)
 
     x_plot=np.linspace(-4, 4, 40)
     y_poly=np.polyval(fit_poly, x_plot)
@@ -36,7 +24,7 @@ def fit():
     print("Curve_fit coeff:      ", fit_curve[0])
 
     plt.figure()
-    plt.plot(d.x,d.y,"k*", label="Actual values")
+    plt.plot(x,y,"k*", label="Actual values")
     plt.plot(x_plot, y_poly, "b-", label="Poly fit")
     plt.plot(x_plot, y_curve, "r--", label="Curve_fit")
     plt.xlabel("X")
