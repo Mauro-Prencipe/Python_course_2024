268cb163ec8cd1df3f858cd32850b8693325a930
Mauro-Prencipe <49814184+Mauro-Prencipe@users.noreply.github.com>
2024-01-26 14:45:04 +0100

    Add files via upload

--- exercise/exercise_complete_advanced.py
+++ exercise/exercise_complete_advanced.py
@@ -1,26 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import random
+import pandas as pd
 from scipy.optimize import curve_fit
 
 
 class Fit():
     
-    dpi=200
+    dpi=80
     
     def __init__(self):
         self.degree=3
         self.file=None
         self.x=None
         self.y=None
+        self.size=None
         self.fit=None
         self.flag=False
         self.read_flag=False
         
+    @classmethod    
+    def set_dpi(cls, dpi):
+        cls.dpi=dpi
+        
+    def set_degree(self, degree):
+        self.degree=degree
+        
     def read_data(self, file_name):
         data=np.loadtxt(file_name)
+        self.file=file_name
         self.x=data[:,0]
         self.y=data[:,1]
+        self.size=len(self.x)
         self.read_flag=True
         
     def polyfit(self):
@@ -52,7 +62,7 @@ class Fit():
         x_plot=np.linspace(xmin, xmax, npoint)
         y_poly=np.polyval(self.fit, x_plot)
         
-        plt.figure(self.dpi)
+        plt.figure(dpi=self.dpi)
         plt.plot(self.x,self.y,"k*", label="Actual values")
         plt.plot(x_plot, y_poly, "b-", label="Poly fit")
         plt.xlabel("X")
@@ -61,11 +71,27 @@ class Fit():
         plt.show()
         
     
+    def table(self):
+        yc=np.polyval(self.fit, self.x)
+        diff=self.y-yc
+        serie=[self.x.round(2), self.y.round(2), yc.round(2), diff.round(2)]
+        
+        data_frame=pd.DataFrame(serie, index=['X ', 'Y ', 'Yc ', 'Delta'])
+        data_frame=data_frame.T
+        print("\n")
+        print(data_frame.to_string(index=False), "\n")
+        
+    def info(self):
+        print("\nData set file:  %s     ", self.file)
+        print("Number of data:  %i3   ", self.size)
+        if self.flag==True:
+            print("Polynomial fit coeff's ", self.fit)
 
-if __name__ == '__main__' :
-    my_data=Fit()
-    my_data.read_data('exercise_data.dat')
-    my_data.plot()
-
+my_data=Fit()
+my_data.set_dpi(100)
+my_data.read_data('exercise_data.dat')
+my_data.plot()
+my_data.table()
+my_data.info()
   
 
